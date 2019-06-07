80817a609ce4a3d047e42f7d5a5825fb277d0d19
Alexey Larionov <alex_osoboe@mail.ru>
2019-06-07 15:33:46 +0300

    CCNA exam 8 debug

---
+++ CCNA Hack (IV-2018)/CCNA 2/inject-eng.js
@@ -0,0 +1,49 @@
+var $;
+(function() {
+    // Load the script
+    var script = document.createElement("script");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        $ = window.jQuery;
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
+var english_quiz;
+$.get( "https://raw.githubusercontent.com/laralex/University-Small-Stuff/master/CCNA%20Hack%20(IV-2018)/CCNA%202/quiz8.json", function( data ) {
+	english_quiz = JSON.parse(data); //entire page in html string
+});
+
+
+var questionElement = document.getElementsByClassName("coreContent")[0];
+
+var answersElements = document.getElementsByClassName("rTableOptionCell");
+
+var questionText = questionElement.innerText;
+
+var finalAnswers;
+
+for (var question in Object.keys(english_quiz)) {
+	if (questionText.indexOf(question.substring(0, 40)) != -1) {
+		var answersGuess = english_quiz[question];
+		if (answersGuess.every( function(ag) {
+			for (var answElem in answersElements) {
+				if (answElem.innerText == ag) return true;	
+			}
+			return false;
+		})
+		finalAnswers = answersGuess;
+	}	
+}
+
+questionElement.innerText += ' Распознано: [ ';
+for (var answ in finalAnswers) {
+	questionElement.innerText += '\'' + answ + '\' ,';
+}
+questionElement.innerText += ' ]\n';
+
+
+
+
+
